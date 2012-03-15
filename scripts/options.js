bdf4eb8b369ced704956faaedd1c255fdf63b97d
houkanshan <houkanshan@gmail.com>
2012-03-16 03:53:15 +0800

    searchfriends

--- scripts/options.js
+++ scripts/options.js
@@ -1,6 +1,6 @@
 putFriend = function(friend){
     getElementById();
-}
+};
 
 searchFriends = function(input){
     var text = input.value.replace(/[\(\)\[\]\{\}\?\*\.\+\$\^\\\| ]/g, '');
@@ -9,6 +9,8 @@ searchFriends = function(input){
     var matchFriends = [];
     var reg = new RegExp(text);
 
+    var ul = document.getElementById('friend_list').getElementsByTagName('ul')[0];
+    ul.innerHTML = "";
     for (var i in allFriends){
         try{
             if (allFriends[i].name.match(text)){
@@ -18,17 +20,30 @@ searchFriends = function(input){
             console.log('[err]searchFriends:' + e);
         }
     }
-    var out = JSON.stringify(matchFriends);
-    console.log(out);
-    document.getElementById('test').innerHTML = out;
+    // var out = JSON.stringify(matchFriends);
+    for(var j in matchFriends){
+        var li =
+        '<li>'+
+            '<a class="head" href="#">'+
+                '<img class="head" src="'+
+                matchFriends[j].tinyurl+
+                '" alt="head">'+
+            '</a>'+
+            '<a class="name" href="#">'+
+            matchFriends[j].name+
+            '</a>'+
+        '</li>';
+        ul.innerHTML += li;
+        console.log(li);
+    }
 
-}
+};
 
 window.onload = function(){
     if(!localStorage.friends){
         ydgz_RR.api({
-            'method': 'friends.getFriends'
-            ,'fields': 'headurl_with_logo,tinyurl_with_logo'
+            'method': 'friends.getFriends',
+            'fields': 'headurl_with_logo,tinyurl_with_logo'
         }, function(res){
             if(!res.err_code){
                 localStorage.friends = JSON.stringify(res);
@@ -39,4 +54,4 @@ window.onload = function(){
             }
         });
     }
-}
+};
